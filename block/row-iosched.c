73adf910d8c4507b8cbe5d325a56040042931bdc
Pranav Vashi <neobuddy89@gmail.com>
2013-05-09 07:57:39 +0300

    block: ROW: Restore idling time

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -80,9 +80,9 @@ static const int queue_quantum[] = {
 	1	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
-/* Default values for idling on read queues */
-#define ROW_IDLE_TIME_MSEC 10	/* msec */
-#define ROW_READ_FREQ_MSEC 25	/* msec */
+/* Default values for idling on read queues (in msec) */
+#define ROW_IDLE_TIME_MSEC 5
+#define ROW_READ_FREQ_MSEC 20
 
 /**
  * struct rowq_idling_data -  parameters for idling on the queue
