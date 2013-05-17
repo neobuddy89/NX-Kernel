82d91ea80593c8a4ded5081773ff04a59afc4c6e
Pranav Vashi <neobuddy89@gmail.com>
2013-05-17 20:27:16 +0300

    init: Reduce logging and remove ftrace init

--- init/main.c
+++ init/main.c
@@ -194,13 +194,13 @@ EXPORT_SYMBOL(loops_per_jiffy);
 
 static int __init debug_kernel(char *str)
 {
-	console_loglevel = 10;
+	console_loglevel = 0;
 	return 0;
 }
 
 static int __init quiet_kernel(char *str)
 {
-	console_loglevel = 4;
+	console_loglevel = 0;
 	return 0;
 }
 
@@ -619,8 +619,6 @@ asmlinkage void __init start_kernel(void)
 
 	acpi_early_init(); /* before LAPIC and SMP init */
 	sfi_init_late();
-
-	ftrace_init();
 
 	/* Do the rest non-__init'ed, we're now alive */
 	rest_init();
